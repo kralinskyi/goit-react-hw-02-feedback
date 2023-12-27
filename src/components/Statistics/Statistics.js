20cdf27d799814921b080d169e2ea9ece7791372
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-12-27 14:05:28 +0200

    done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,12 +5,12 @@ import css from './Statistics.module.css';
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <ul className={css.statistics}>
-      <li className={css.item}>good: {good}</li>
-      <li className={css.item}>neutral: {neutral}</li>
-      <li className={css.item}>bad: {bad}</li>
-      <li className={css.item}>total: {total}</li>
+      <li className={css.item}>Good: {good}</li>
+      <li className={css.item}>Neutral: {neutral}</li>
+      <li className={css.item}>Bad: {bad}</li>
+      <li className={css.item}>Total: {total}</li>
       {total ? (
-        <li className={css.item}>positive: {positivePercentage} %</li>
+        <li className={css.item}>Positive: {positivePercentage} %</li>
       ) : null}
     </ul>
   );
